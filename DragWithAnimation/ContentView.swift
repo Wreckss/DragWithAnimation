672c7db14b277f6a172fb29bcb037087230f03ba
wreckss <46939881+Wreckss@users.noreply.github.com>
2020-08-04 17:42:39 -0600

    Draggable interactive animations

--- DragWithAnimation/ContentView.swift
+++ DragWithAnimation/ContentView.swift
@@ -5,13 +5,52 @@
 //  Created by taco on 8/4/20.
 //  Copyright © 2020 Wrecks. All rights reserved.
 //
+/*
+ https://www.hackingwithswift.com/books/ios-swiftui/animating-gestures
+ */
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var dragAmount = CGSize.zero
+    
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    
     var body: some View {
-        Text("Hello, World!")
-    }
+//        LinearGradient(gradient: Gradient(colors: [.yellow,.red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .frame(width: 300, height: 200)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+//            .offset(dragAmount)
+//        .gesture(
+//            DragGesture()
+//                .onChanged { self.dragAmount = $0.translation }
+//                .onEnded { _ in withAnimation(.spring()) {
+//                    self.dragAmount = .zero
+//                    }
+//                }
+//            )
+        
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                .padding(5)
+                    .font(.title)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num) / 20))
+            }
+        }
+    .gesture(
+        DragGesture()
+            .onChanged { self.dragAmount = $0.translation }
+            .onEnded { _ in
+                self.dragAmount = .zero
+                self.enabled.toggle()
+        }
+        )
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
